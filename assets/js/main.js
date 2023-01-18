e874449c5deb9af1887d2bebdc3f7a609eb0cf63
aya-thafer2 <ayathafer2@gmail.com>
2023-01-18 19:59:54 +0200

    edit gallery and interested

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,7 +39,6 @@ ScrollReveal({
     delay:200
 });
 ScrollReveal().reveal('.gallery .gallery-title,.interested .interested-title', { delay: 200, origin:'left' });
-// ScrollReveal().reveal('.gallery img', { delay: 300, origin:'bottom'});
 ScrollReveal().reveal('.interested .cardL', { delay: 400, origin:'left'});
 ScrollReveal().reveal('.interested .cardC', { delay: 400, origin:'bottom'});
 ScrollReveal().reveal('.interested .cardR', { delay: 400, origin:'right'});
